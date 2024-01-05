a834a06f7cd178fdd50affa74934922cb41e1e09
tlaw22 <timothy.j.lawless@youtlook.com>
2024-01-05 10:40:15 -0500

    try with resources

--- src/App.java
+++ src/App.java
@@ -4,12 +4,9 @@ public class App {
     public static void main(String[] args) {
 
         File file = new File("myfile.txt");
-        BufferedReader reader = null;
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(file);
-            reader = new BufferedReader(fileReader);
-
+        try (FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+        ){
             String line = reader.readLine();
             while (line != null) {
                 System.out.println(line);
@@ -21,19 +18,6 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problem reading the file " + file.getName());
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-                
-            } catch (IOException e) {
-                System.out.println("Unable to close file" + file.getName());
-                e.printStackTrace();
-            }
         }
     }
 }
